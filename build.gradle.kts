3acf251943ed40cdf1a0026dc01b058a8e6c8c3f
MaxBuster380 <maxbuster380@gmail.com>
2024-03-22 16:06:02 +0100

    Init Commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose")
+}
+
+group = "com.github.MaxBuster380"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven ("https://jitpack.io")
+    google()
+}
+
+dependencies {
+    // Note, if you develop a library, you should use compose.desktop.common.
+    // compose.desktop.currentOs should be used in launcher-sourceSet
+    // (in a separate module for demo project and in testMain).
+    // With compose.desktop.common you will also lose @Preview functionality
+    implementation("com.github.MaxBuster380:BackwardRegex:alpha-1.0.0")
+    implementation(compose.desktop.currentOs)
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "BackwardRegex-Desktop"
+            packageVersion = "1.0.0"
+        }
+    }
+}
