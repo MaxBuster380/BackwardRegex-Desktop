2d02d0bb6752dec33e747920a4239b91ed3941f5
MaxBuster380 <maxbuster380@gmail.com>
2024-03-22 17:03:40 +0100

    Second implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,8 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,7 +11,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import org.example.compiler.BackwardRegexCompiler
@@ -26,6 +24,7 @@ fun App() {
     val nbElements = 15
 
     var textualRegex by remember { mutableStateOf("") }
+    var regex by remember { mutableStateOf( Regex("") ) }
     var compiledRegex by remember { mutableStateOf(compiler.generate("".toRegex())) }
 
     MaterialTheme {
@@ -35,35 +34,29 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Column(
-                modifier = Modifier.fillMaxWidth(0.75F)
+                modifier = Modifier
+                    .fillMaxWidth(0.75F)
             ) {
 
                 Row(
                     modifier = Modifier
-                        .fillMaxHeight(0.3F)
+                        .fillMaxHeight(0.2F)
                 ) {
                     Box{
                         TextField(
                             value = textualRegex,
                             onValueChange = { textualRegex = it },
                             placeholder = { Text("Enter a regular expression.") },
-                            isError = try {
-                                Regex(textualRegex); false
-                            } catch (_: Exception) {
-                                true
-                            },
+                            isError = !isRegexValid( textualRegex ),
                             modifier = Modifier.fillMaxWidth(0.8F)
                         )
                     }
                     Button(
                         onClick = {
-                            compiledRegex = compiler.generate(textualRegex.toRegex())
+                            regex = Regex(textualRegex)
+                            compiledRegex = compiler.generate(regex)
                         },
-                        enabled = try {
-                            Regex(textualRegex); true
-                        } catch (_: Exception) {
-                            false
-                        }
+                        enabled = isRegexValid( textualRegex )
                     ) {
                         Text("Generate")
                     }
@@ -71,31 +64,61 @@ fun App() {
 
                 Spacer(
                     modifier = Modifier
-                        .fillMaxHeight(0.05F)
+                        .fillMaxHeight(0.025F)
                 )
 
                 LazyColumn(
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally,
                     modifier = Modifier
-                        .fillMaxHeight(0.65F)
+                        .fillMaxHeight(0.775F)
                         .fillMaxWidth()
 
                 ) {
                     for (i in 1..nbElements) {
                         item {
-                            Box(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .border(1.dp, Color.Red),
-                                contentAlignment = Alignment.Center
-                            ) {
-                                Text(compiledRegex.generateMatchingText())
-                            }
+                            GeneratedTextItem(
+                                compiledRegex.generateMatchingText(),
+                                regex
+                            )
                         }
                     }
                 }
             }
+        }
+    }
+}
+
+fun isRegexValid(text : String) : Boolean = try {
+    Regex(text); true
+} catch (_: Exception) {
+    false
+}
+
+@Composable
+fun GeneratedTextItem(text : String, regex : Regex) {
+    val color = if ( regex.matches(text) ) {
+        Color.DarkGray
+    } else {
+        Color.Red
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+
+            Text(
+                text,
+                modifier = Modifier
+                    .fillMaxWidth(0.8F),
+                textAlign = TextAlign.Center,
+                style = TextStyle(color = color)
+            )
         }
     }
 }
